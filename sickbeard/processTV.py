5b3f85d259f078729c1d8f337f3e154729920873
schumi2004 <schumi2004@gmail.com>
2013-05-22 20:28:23 +0200

    Redo PostProcessor Fixes for archive downloads, doesnt solve issue

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -107,7 +107,6 @@ def processDir (dirName, nzbName=None, recurse=False, failed=False):
             files = ek.ek(os.listdir, dirName)
             dirs = [dirs]
 
-    process_result = False
     videoFiles = filter(helpers.isMediaFile, files)
 
     # If nzbName is set and there's more than one videofile in the folder, files will be lost (overwritten).
@@ -143,7 +142,7 @@ def processDir (dirName, nzbName=None, recurse=False, failed=False):
     #Process Video File in all TV Subdir
     for dir in [x for x in dirs if validateDir(path, x, returnStr)]:
 
-        process_result = False
+        process_result = True
 
         for processPath, processDir, fileList in ek.ek(os.walk, ek.ek(os.path.join, path, dir), topdown=False):
 
@@ -233,14 +232,14 @@ def validateDir(path, dirName, returnStr):
             return False
 
     #check if the dir have at least one tv video file
-#    files = ek.ek(os.listdir, os.path.join(path, dirName))
-#    videoFiles = filter(helpers.isMediaFile, files)
-#    
-#    for video in videoFiles:
-#        try:
-#            NameParser().parse(video)
-#            return True
-#        except InvalidNameException:
-#            pass
+    files = ek.ek(os.listdir, os.path.join(path, dirName))
+    videoFiles = filter(helpers.isMediaFile, files)
+    
+    for video in videoFiles:
+        try:
+            NameParser().parse(video)
+            return True
+        except InvalidNameException:
+            pass
 
-    return True
+    return False
