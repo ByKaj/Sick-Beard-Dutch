5e79a631ea0a13cfc232c3d93d5dd12d1b735946
schumi2004 <schumi2004@gmail.com>
2013-07-27 02:14:28 +0200

    Make processTV a bit easier to read

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -23,7 +23,7 @@ import shutil
 
 import sickbeard
 from sickbeard import postProcessor
-from sickbeard import db, helpers, exceptions, show_name_helpers
+from sickbeard import db, helpers, exceptions
 
 from sickbeard import encodingKludge as ek
 from sickbeard.exceptions import ex
@@ -31,7 +31,6 @@ from sickbeard.exceptions import ex
 from sickbeard import logger
 
 from sickbeard import failedProcessor
-from sickbeard import failed_history
 
 from sickbeard.name_parser.parser import NameParser, InvalidNameException
 
@@ -39,6 +38,89 @@ from sickbeard.name_parser.parser import NameParser, InvalidNameException
 def logHelper (logMessage, logLevel=logger.MESSAGE):
     logger.log(logMessage, logLevel)
     return logMessage + u"\n"
+
+def _processFailed(dirName, nzbName):
+    """Process a download that did not complete correctly"""
+
+    returnStr = u""
+    try:
+        processor = failedProcessor.FailedProcessor(dirName, nzbName)
+        process_result = processor.process()
+        process_fail_message = ""
+    except exceptions.FailedProcessingFailed, e:
+        process_result = False
+        process_fail_message = ex(e)
+
+    returnStr += processor.log
+
+    if sickbeard.DELETE_FAILED and process_result:
+        returnStr += logHelper(u"Deleting folder of failed download " + dirName, logger.DEBUG)
+        try:
+            shutil.rmtree(dirName)
+        except (OSError, IOError), e:
+            returnStr += logHelper(u"Warning: Unable to remove the failed folder " + dirName + ": " + ex(e), logger.WARNING)
+
+    if process_result:
+        returnStr += logHelper(u"Processing succeeded: (" + str(nzbName) + ", " + dirName + ")")
+    else:
+        returnStr += logHelper(u"Processing failed: (" + str(nzbName) + ", " + dirName + "): " + process_fail_message, logger.WARNING)
+
+    return returnStr
+
+def _processNormal(dirName, nzbName, recurse=False):
+    """Process a download that completed without issue"""
+
+    returnStr = u""
+    fileList = ek.ek(os.listdir, dirName)
+
+    # split the list into video files and folders
+    folders = filter(lambda x: ek.ek(os.path.isdir, ek.ek(os.path.join, dirName, x)), fileList)
+    videoFiles = filter(helpers.isMediaFile, fileList)
+
+    # recursively process all the folders
+    for curFolder in folders:
+        returnStr += logHelper(u"Recursively processing a folder: " + curFolder, logger.DEBUG)
+        returnStr += _processNormal(ek.ek(os.path.join, dirName, curFolder), recurse=True)
+
+    remainingFolders = filter(lambda x: ek.ek(os.path.isdir, ek.ek(os.path.join, dirName, x)), fileList)
+
+    # If nzbName is set and there's more than one videofile in the folder, files will be lost (overwritten).
+    if nzbName is not None and len(videoFiles) >= 2:
+        nzbName = None
+
+    # process any files in the dir
+    for cur_video_file_path in videoFiles:
+        cur_video_file_path = ek.ek(os.path.join, dirName, cur_video_file_path)
+        try:
+            processor = postProcessor.PostProcessor(cur_video_file_path, nzbName)
+            process_result = processor.process()
+            process_fail_message = ""
+        except exceptions.PostProcessingFailed, e:
+            process_result = False
+            process_fail_message = ex(e)
+
+        returnStr += processor.log
+
+        # as long as the postprocessing was successful delete the old folder unless the config wants us not to
+        if not process_result:
+            returnStr += logHelper(u"Processing failed for " + cur_video_file_path + ": " + process_fail_message, logger.WARNING)
+        else:
+            if len(videoFiles) == 1 and not sickbeard.KEEP_PROCESSED_DIR and \
+                ek.ek(os.path.normpath, dirName) != ek.ek(os.path.normpath, sickbeard.TV_DOWNLOAD_DIR) and \
+                    len(remainingFolders) == 0:
+
+                returnStr += logHelper(u"Deleting folder " + dirName, logger.DEBUG)
+
+                try:
+                    shutil.rmtree(dirName)
+                except (OSError, IOError), e:
+                    returnStr += logHelper(u"Warning: unable to remove the folder " + dirName + ": " + ex(e), logger.WARNING)
+
+            returnStr += logHelper(u"Processing succeeded for " + cur_video_file_path)
+
+        return returnStr
+
+
 
 def processDir (dirName, nzbName=None, recurse=False, failed=False):
     """
@@ -71,29 +153,6 @@ def processDir (dirName, nzbName=None, recurse=False, failed=False):
         returnStr += logHelper(u"Unable to figure out what folder to process. If your downloader and Sick Beard aren't on the same PC make sure you fill out your TV download dir in the config.", logger.DEBUG)
         return returnStr
 
-    if failed:
-        try:
-            processor = failedProcessor.FailedProcessor(dirName, nzbName)
-            process_result = processor.process()
-            process_fail_message = ""
-        except exceptions.FailedProcessingFailed, e:
-            process_result = False
-            process_fail_message = ex(e)
-
-        returnStr += processor.log 
-
-        if sickbeard.DELETE_FAILED and process_result:
-            returnStr += logHelper(u"Deleting folder of failed download " + dirName, logger.DEBUG)
-            try:
-                shutil.rmtree(dirName)
-            except (OSError, IOError), e:
-                returnStr += logHelper(u"Warning: Unable to remove the failed folder " + dirName + ": " + ex(e), logger.WARNING)
-
-        if process_result:
-            returnStr += logHelper(u"Processing succeeded: (" + str(nzbName) + ", " + dirName + ")")
-        else:
-            returnStr += logHelper(u"Processing failed: (" + str(nzbName) + ", " + dirName + "): " + process_fail_message, logger.WARNING)
-        return returnStr
 # Keep it in case we need it for manual failed download PP. Just drop old
 # records as part of the monthly purge.
 #    else:
