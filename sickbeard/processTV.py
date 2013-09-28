8fae9bff33ef0e3db4ea4fe955e5661ac403265d
schumi2004 <schumi2004@gmail.com>
2013-09-28 21:05:33 +0200

    Update processTV.py

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -256,7 +256,6 @@ def processDir (dirName, nzbName=None, recurse=False, failed=False):
                     continue
 
                 returnStr += logHelper(u"Deleting folder " + processPath, logger.DEBUG)
-                returnStr += logHelper(u"Processing succeeded for "+cur_video_file_path)
 
                 try:
                     shutil.rmtree(processPath)
